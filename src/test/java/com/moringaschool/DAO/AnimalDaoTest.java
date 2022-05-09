48fd0ef7684a5b849e54a3070bab8559ed3c0905
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-09 11:25:45 +0300

    patch: It will create AnimalDaoTest class

---
+++ src/test/java/com/moringaschool/DAO/AnimalDaoTest.java
@@ -0,0 +1,17 @@
+package com.moringaschool.DAO;
+
+import org.junit.Before;
+import org.sql2o.Connection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimalDaoTest {
+    private AnimalDao animalDao;
+    private Connection con;
+
+    @Before
+    public void setup() throws Exception{
+        
+    }
+
+}
