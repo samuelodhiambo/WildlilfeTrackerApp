cdc21778ce97925317136dc1f3e51fba88eac260
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-09 11:25:04 +0300

    patch: It will create Animal DAO Interface

--- src/main/java/com/moringaschool/DAO/AnimalDaoInterface.java
+++ src/main/java/com/moringaschool/DAO/AnimalDaoInterface.java
@@ -1,4 +1,12 @@
 package com.moringaschool.DAO;
 
+import com.moringaschool.Models.Animal;
+import org.sql2o.Connection;
+
+import java.util.List;
+
 public interface AnimalDaoInterface {
+    void add(Connection con, Animal animal);
+
+    List<Animal> findAll(Connection con);
 }
