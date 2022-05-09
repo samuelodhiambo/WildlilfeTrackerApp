a6f34cc15fe3f4cf6ce5e67be1a12912e4f5757a
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-09 11:26:32 +0300

    patch: It will create EndangeredAnimals class

--- src/main/java/com/moringaschool/Models/EndangeredAnimals.java
+++ src/main/java/com/moringaschool/Models/EndangeredAnimals.java
@@ -1,4 +1,29 @@
 package com.moringaschool.Models;
 
+import java.util.ArrayList;
+
 public class EndangeredAnimals {
+    private int id;
+    private String name;
+    private String health;
+    public static ArrayList<EndangeredAnimals> instances;
+
+    public EndangeredAnimals(String name, String health) {
+        this.health = health;
+        this.name = name;
+        instances.add(this);
+        this.id = getInstances().size();
+    }
+
+    public static ArrayList<EndangeredAnimals> getInstances() {
+        return instances;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHealth() {
+        return health;
+    }
 }
