1040d7ee00ab1e59ee48405feb725e729d473a08
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-09 11:24:22 +0300

    patch: It will implement Animal Data access object

--- src/main/java/com/moringaschool/DAO/AnimalDao.java
+++ src/main/java/com/moringaschool/DAO/AnimalDao.java
@@ -1,4 +1,34 @@
 package com.moringaschool.DAO;
 
+import com.moringaschool.Models.Animal;
+
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
 public class AnimalDao implements AnimalDaoInterface{
+    @Override
+    public void add(Connection con, Animal animal) {
+        String sql = "INSERT INTO animals (id, name) VALUES (:id, :name)";
+        try{
+            int id = (int) con.createQuery(sql, true)
+                    .bind(animal) //use name and location from match object for sql
+                    .executeUpdate()
+                    .getKey();
+            animal.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println("there was a problem adding the animal");
+        }
+    }
+
+    @Override
+    public List<Animal> findAll(Connection con) {
+        try{
+            return con.createQuery("SELECT * FROM matches")
+                    .executeAndFetch(Animal.class);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
 }
