cca96d05659227cc4eac3078d6d8bae095873162
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-10 13:29:03 +0300

    patch: It will add home page route

--- src/main/java/com/moringaschool/Main.java
+++ src/main/java/com/moringaschool/Main.java
@@ -1,7 +1,38 @@
 package com.moringaschool;
+import com.moringaschool.DAO.AnimalDao;
+import com.moringaschool.Models.Animal;
+import org.sql2o.Connection;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+import static spark.SparkBase.port;
+import static spark.SparkBase.staticFileLocation;
 
 public class Main {
+    static Connection con;
+    static AnimalDao animalDao;
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+
+//            animalDao.add(new Animal("Rhino"));
+//            animalDao.add(new Animal("Elephant"));
+
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
