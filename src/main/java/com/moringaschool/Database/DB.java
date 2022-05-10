3dbdd52ee7b03c816702bdf2a61cdf7c45860d5e
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-10 23:29:57 +0300

    patch: It will add method to create tables

--- src/main/java/com/moringaschool/Database/DB.java
+++ src/main/java/com/moringaschool/Database/DB.java
@@ -1,7 +1,19 @@
 package com.moringaschool.Database;
 
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/WildlifeTrackerApp", "samian" ,"");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/samian", "samian" ,"root");
+
+
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-172-175-251.compute-1.amazonaws.com:5432/d1mpdg2996qfh9", "sbiayrlhnszans" ,"574967f50e3658f1004a13f61dfd0241e1c20a08436880f34993edaad7a2eafd");
+
+    public static void createTables(Connection con) {
+        try {
+            con.createQuery("CREATE TABLE IF NOT EXISTS animals (id SERIAL PRIMARY KEY, name VARCHAR UNIQUE) ").executeUpdate();
+        } catch (Exception err) {
+            throw new RuntimeException(err);
+        }
+    }
 }
