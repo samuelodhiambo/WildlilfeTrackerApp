0a27a76024f62d6fb66110327a3959f58fb7d002
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-10 23:33:23 +0300

    patch: It will add db connection

--- src/main/java/com/moringaschool/Main.java
+++ src/main/java/com/moringaschool/Main.java
@@ -1,21 +1,24 @@
 package com.moringaschool;
 import com.moringaschool.DAO.AnimalDao;
+import com.moringaschool.Database.DB;
 import com.moringaschool.Models.Animal;
 import org.sql2o.Connection;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
 
 import static spark.SparkBase.port;
 import static spark.SparkBase.staticFileLocation;
+import org.sql2o.*;
 
 public class Main {
-    static Connection con;
-    static AnimalDao animalDao;
+    static Connection con = DB.sql2o.open();
+    static AnimalDao animalDao = new AnimalDao(DB.sql2o);
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
@@ -24,13 +27,18 @@ public class Main {
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
     public static void main(String[] args) {
+        DB.createTables(con);
         port(getHerokuAssignedPort());
         staticFileLocation("/public");
         get("/", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
+            Map<String, List<Animal>> model = new HashMap<>();
 
-//            animalDao.add(new Animal("Rhino"));
-//            animalDao.add(new Animal("Elephant"));
+            animalDao.add(new Animal("Rhino"));
+            animalDao.add(new Animal("Elephant"));
+
+            model.put("animals", animalDao.findAll(con));
+
+//            System.out.println(animalDao.findAll(con));
 
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
